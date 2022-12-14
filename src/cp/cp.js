c7f5239d035b32012afa8be8af99239e3c852a02
Max Tyshkevich <2842778@gmail.com>
2022-12-14 19:33:57 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,17 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import { fork } from 'node:child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathFile = join(__dirname, './files/script.js');
+  const child_process = fork(pathFile, args, { silent: true });
+
+  process.stdin.pipe(child_process.stdin);
+
+  child_process.stdout.pipe(process.stdout);
 };
 
 spawnChildProcess();
